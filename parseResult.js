aef32977ae186e1e2596b419b34ee4fcc84ba6cc
David Burke <david.burke@aakonsult.com>
2024-02-12 09:05:51 +1100

    Removed test coverage as results not accurate

--- parseResult.js
+++ parseResult.js
@@ -20,10 +20,10 @@ if (firstPart) {
 let summaryText = '';
 
 if(summary.outcome == 'Failed'){
-     summaryText = `❌   Automated unit testing for ${summary.username} (${hostname}) has *${summary.outcome}* with ${summary.testsRan} test runs and ${summary.failing} failure(s). Code coverage: ${summary.testRunCoverage}`
+     summaryText = `❌   Automated unit testing for ${summary.username} (${hostname}) has *${summary.outcome}* with ${summary.testsRan} test runs and ${summary.failing} failure(s).`
 }
 else{
-    summaryText = `✅   Automated unit testing for ${summary.username} (${hostname}) has *${summary.outcome}* 🎉. Code coverage: ${summary.testRunCoverage}`
+    summaryText = `✅   Automated unit testing for ${summary.username} (${hostname}) has *${summary.outcome}* 🎉.`
 }
 
 
